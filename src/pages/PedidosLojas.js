39414ba382533ae7a02670705d189babc2a8ebfe
GuilhermePellegrini <pellegrini458@gmail.com>
2021-12-15 20:32:27 -0300

    Update PedidosLojas.js

--- src/pages/PedidosLojas.js
+++ src/pages/PedidosLojas.js
@@ -7,7 +7,7 @@ import styles from "../pages/PedidosLojas.module.css"
 const PedidosLojas = () => {
 
   const [pedidos, setPedidos] = useState([]);
-
+  const [update, setUpdate] = useState(true);
     const params = useParams();
     console.log(params);
 
@@ -25,23 +25,102 @@ const PedidosLojas = () => {
               };
 
               const { data} = await api.get(`pedidos/loja/${params.id}`, config);
+              console.log(data.pedidos);
               setPedidos(data.pedidos);
         }
         getPedidos();
     }, []);
 
-    console.log(pedidos);
+    useEffect(() => {
+      async function getPedidos() {
+          
+          let config = {
+              method: "get",
+              headers: {
+                Accept: "application/json",
+                Authorization: `Bearer ${token}`,
+              },
+            };
+
+            const { data} = await api.get(`pedidos/loja/${params.id}`, config);
+            console.log(data.pedidos);
+            setPedidos(data.pedidos);
+      }
+      getPedidos();
+      console.log('att');
+  }, [update]);
+
+    async function aceitarPedido(id_pedido) {
+      let config = {
+        method: "get",
+        headers: {
+          Accept: "application/json",
+          Authorization: `Bearer ${token}`,
+        },
+      };
+      let dataAxios = {
+        status: "Aceito"
+      }
+      const { dataAceita } = await api.put(`pedido/${id_pedido}`, dataAxios, config);
+      setUpdate(!update);
+    }
+
+    
+    async function recusarPedido(id_pedido) {
+      let config = {
+        method: "get",
+        headers: {
+          Accept: "application/json",
+          Authorization: `Bearer ${token}`,
+        },
+      };
+      let dataAxios = {
+        status: "Cancelado"
+      }
+      const { dataRecusa } = await api.put(`pedido/${id_pedido}`, dataAxios, config);
+      setUpdate(!update);
+    }
+
+    async function entregaPedido(id_pedido) {
+      let config = {
+        method: "get",
+        headers: {
+          Accept: "application/json",
+          Authorization: `Bearer ${token}`,
+        },
+      };
+      let dataAxios = {
+        status: "Entrega"
+      }
+      const { dataRecusa } = await api.put(`pedido/${id_pedido}`, dataAxios, config);
+      setUpdate(!update);
+    }
+
   return (
     <div className={styles.pedidos}>
       <h1>Pedidos</h1>
-      {pedidos.length === 0 && <p>Não existem pedidos nesta loja.</p>}
-      {pedidos.map((pedido) => (
-        <div key={pedido.pedido.id} className={styles.pedido}>
-          <p>ID do Pedido: {pedido.pedido.id}</p>
-          <p>Preço: R${pedido.pedido.price.toFixed(2).replace(".", ",")}</p>
-          <p>Status: {pedido.pedido.status === "Pagamento_Pendente" ? "Pagamento Pendente" : pedido.pedido.status}</p>
-        </div>
-      ))}
+      {pedidos.length === 0 ? <p>Não existem pedidos nesta loja.</p> :
+        pedidos.map((pedido) => (
+          pedido.status != "Pagamento_Pendente" &&
+          <div key={pedido.id} className={styles.pedido}>
+            <p>ID do Pedido: {pedido.id}</p>
+            <p>Preço: R${pedido.price.toFixed(2).replace(".", ",")}</p>
+            <p>Status: {pedido.status === "Pagamento_Pendente" ? "Pagamento Pendente" : pedido.status}</p>
+            {pedido.status === 'Pendente' ? 
+              <>
+                <button onClick={() => aceitarPedido(pedido.id)} >Aceitar Pedido</button>
+                <button onClick={() => recusarPedido(pedido.id)} >Recusar Pedido</button>
+              </>
+              :
+                pedido.status != 'Cancelado' && pedido.status == 'Aceito' &&
+                <>
+                  <button onClick={() => recusarPedido(pedido.id)} >Cancelar Pedido</button>
+                  <button onClick={() => entregaPedido(pedido.id)} >Informar Envio</button>
+                </>
+            }
+          </div>
+        ))
+      }
     </div>
   );
 };
